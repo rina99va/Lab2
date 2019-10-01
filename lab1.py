49534d3ead2eb9f98e72bf132365f56492cc3f2f
Petar <ptangra@abv.bg>
2019-10-01 22:34:39 +0300

    added new feature to your Calculator class by P.Georgiev

--- lab1.py
+++ lab1.py
@@ -11,6 +11,12 @@ class Calculator:
         num1 = int(input("Enter first number:"))
         num2 = int(input("Enter second number:"))
         return num1 + num2
+
+    def Add3(self):
+        """Function for adding numbers"""
+        num1 = int(input("Enter first number:"))
+        num2 = int(input("Enter second number:"))
+        return num1 + num2 + 3
     
     def Subtract(self):
         num1 = int(input("Enter first number:"))
